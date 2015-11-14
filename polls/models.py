4b8d8c7830d6d279c38443b5e2c6b06edcfb1385
lizuyao2010 <zuyaoli2010@gmail.com>
2015-11-14 10:23:45 -0800

    add course model
    please see days field

--- polls/models.py
+++ polls/models.py
@@ -20,4 +20,22 @@ class Student(models.Model):
 	major = models.CharField(max_length = 20)
 
 class Course(models.Model):
-    pass
+    course_id=models.CharField(max_length=30)
+    course_name=models.CharField(max_length=30)
+    course_section=models.CharField(max_length=30)
+    COURSE_TYPE=(
+        ('Le','Lecture'),
+        ('La','Lab'),
+        ('Q','Quiz'),   
+    )
+    course_type=models.CharField(max_length=2,choices=COURSE_TYPE)
+    instructor=models.CharField(max_length=30)
+    location=models.CharField(max_length=30)
+    capcity=models.IntegerField()
+    registered=models.IntegerField()
+    time=models.TimeField()
+    # how to add multiple choices?
+    days=models.CharField(max_length=30)
+    students=models.ManyToManyField(Student)
+
+
